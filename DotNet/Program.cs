092a94c1c9e7969e1364a1477ec3262873cf3adc
suyogrepale <suyogbrepale@gmail.com>
2022-01-29 22:19:00 +0530

    29_01

--- DotNet/Program.cs
+++ DotNet/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace Program // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
